760bc964ff8fabe6d25dd2cc772196f51fb94921
4eburek0v <73301320+4eburek0v@users.noreply.github.com>
2021-01-12 23:56:01 +0200

    Delete newmax directory

--- newmax/js/js.js
+++
@@ -1,9 +0,0 @@
-$('#ua').click(function () { 
-    $('#ua').addClass('lang_link_active');
-    $('#ru').removeClass('lang_link_active');
-});
-
-$('#ru').click(function () { 
-    $('#ru').addClass('lang_link_active');
-    $('#ua').removeClass('lang_link_active');
-});
